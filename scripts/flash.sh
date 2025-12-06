eebd26fa5caf4d4318179ef7c7d08245de816272
bard <bard@bard-server.local>
2025-12-05 20:25:22 -0600

    build working

--- scripts/flash.sh
+++ scripts/flash.sh
@@ -1,3 +1,14 @@
-#!usr/bin/bash
 
-# TODO: implement
+#!/usr/bin/env bash
+set -e
+
+export IDF_TOOLS_PATH=${PWD}/lib/idf-tools
+export IDF_PATH=${PWD}/lib/esp-idf
+
+. ${IDF_PATH}/export.sh
+
+idf.py fullclean
+idf.py set-target esp32s3
+idf.py -b 115200 flash
+
+# idf.py autodetects port but use -p <port> to specify
--- scripts/monitor.sh
+++ scripts/monitor.sh
@@ -1,3 +1,12 @@
-#!usr/bin/bash
 
-# TODO: implement
+#!/usr/bin/env bash
+set -e
+
+export IDF_TOOLS_PATH=${PWD}/lib/idf-tools
+export IDF_PATH=${PWD}/lib/esp-idf
+
+. ${IDF_PATH}/export.sh
+
+idf.py -b 115200 monitor
+
+# idf.py autodetects port but use -p <port> to specify
